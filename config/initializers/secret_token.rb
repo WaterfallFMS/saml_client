fb0bfe1bbd6e0c6f6f05f4cf61817cce225874ad
John Kamenik <jkamenik@gmail.com>
2013-11-25 12:42:40 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SamlClient::Application.config.secret_key_base = '4d6c36c9b7a74102f1c5190ce50075d0e2640b904e213883673bc180bad0fafebc51e3518aa0ec3d4599e00681b61e4c31016f5f462994b8fa0fde443e66a997'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SamlClient::Application.config.session_store :cookie_store, key: '_saml_client_session'
