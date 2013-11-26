b472d7d5df4d86b4910e55573691e62c3a70b5f2
John Kamenik <jkamenik@gmail.com>
2013-11-26 07:54:57 -0500

    Forcing a redirect to SAML authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SamlClient::Application.routes.draw do
   post '/auth/saml/callback' => 'sessions#create'
 
+  root 'application#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
