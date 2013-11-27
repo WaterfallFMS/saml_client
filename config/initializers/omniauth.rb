3e0db01b1acf7b073cae12db9ea74e51bf4056ba
John Kamenik <jkamenik@gmail.com>
2013-11-27 14:13:55 -0500

    Switch to allowing any IdP

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :saml,
-    :issuer                             => "waterfall-saml-client",
-    :idp_sso_target_url                 => "https://app.onelogin.com/trust/saml2/http-post/sso/342922",
-    :idp_cert_fingerprint               => "96:9E:B2:2E:FE:23:19:69:3B:02:58:16:25:DC:2C:27:F8:27:A1:FC",
-    :name_identifier_format             => "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
+    :issuer                             => ENV['SAML_IDP_ISSUER'],
+    :idp_sso_target_url                 => ENV['SAML_IDP_TARGET_URL'],
+    :idp_cert_fingerprint               => ENV['SAML_IDP_CERT_FINGERPRINT'],
+    :name_identifier_format             => ENV['SAML_IDP_NAME_FORMAT']
 end
